917553a731ad3c92b9d1e451f89c4327ee6970c2
NathanielBampo <mergontv@gmail.com>
2023-01-14 12:42:56 +0400

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  require 'sidekiq/web'
+  require 'sidekiq/cron/web'
+  
+  get :health, to: proc { [200, {}, ["OK"]] }
+  
+  get "/weather/current", to: "forecasts#current"
+  get "/weather/historical", to: "forecasts#historical"
+  get "/weather/max", to: "forecasts#max"
+  get "/weather/min", to: "forecasts#min"
+  get "/weather/avg", to: "forecasts#avg"
+  get "/weather/by_time", to: "forecasts#by_time"
 end
