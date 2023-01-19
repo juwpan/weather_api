103157fa50d1c21ec60d6ba4f1394afbf7327db5
NathanielBampo <mergontv@gmail.com>
2023-01-19 14:00:02 +0400

    added rspec test for #forecast_service

--- spec/service/forecast_service_spec.rb
+++ spec/service/forecast_service_spec.rb
@@ -1,12 +1,35 @@
 require 'rails_helper'
 require 'support/factory_bot'
+require 'vcr'
 
-RSpec.describe 'ForecastService', type: :service do
-  describe "" do
-    context "a context" do
-      it "" do
-        
+RSpec.describe ForecastService do
+  let(:forecast_service) { ForecastService.new }
+
+  describe "#historical_forecast" do
+    before(:each) do
+      VCR.use_cassette("historical_forecast") do
+        @forecast = forecast_service.historical_forecast
       end
-    end 
+    end
+
+    it "returns an array of forecast data" do
+      expect(@forecast).to be_an(Array)
+    end
+
+    it "saves the forecast data to the database" do
+      expect(Forecast.count).to be > 0
+    end
+
+    it "saves the correct temperature and epoch_time to the database" do
+      @forecast.each do |data|
+        temp = data['Temperature']['Metric']['Value']
+        time_stamp = data['EpochTime']
+
+        forecast = Forecast.find_by(epoch_time: Time.at(time_stamp))
+
+        expect(forecast.temperature).to eq(temp)
+        expect(forecast.epoch_time).to eq(Time.at(time_stamp))
+      end
+    end
   end
 end
